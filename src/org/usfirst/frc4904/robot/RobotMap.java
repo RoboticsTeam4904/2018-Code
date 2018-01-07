d506b5062ec9bfe10dd4f5b6249c41a793d481c6
billwpierce <wwpierce020602@gmail.com>
2018-01-07 10:59:08 -0800

    Added basic robotmap structure and human inputs

---
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc4904.robot;
+
+public class RobotMap {
+	public static class Port {
+		public static class HumanInput {
+			public static final int joystick = 0;
+			public static final int xboxController = 1;
+		}
+
+		public static class CANMotor {}
+
+		public static class PWM {}
+
+		public static class CAN {}
+
+		public static class Pneumatics {}
+	}
+
+	public static class Metrics {}
+
+	public static class Component {}
+	
+	public RobotMap() {
+	
+	}
+}
