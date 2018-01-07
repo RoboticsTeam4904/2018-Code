8b16298e3777acd20ac985321cc142ae51669449
billwpierce <wwpierce020602@gmail.com>
2018-01-07 15:43:20 -0800

    Initialize nearby switch time auton routine

---
+++ src/org/usfirst/frc4904/autonly/NearSwitchTime.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc4904.autonly;
+
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class NearSwitchTime extends CommandGroup {
+	public static final double TIME_APPROACH_SWITCH = 1.5;
+
+    public NearSwitchTime() {
+        addSequential(new ChassisConstant(RobotMap.Component.chassis, 0, AutonConfig.DEAD_RECKON_DRIVE_SPEED, 0,
+			AutonGearCenterPegTime.TIME_INITIAL_APPROACH))
+    }
+}
