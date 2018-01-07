9e87a0819090f655372ff737575121a740e6aa45
billwpierce <wwpierce020602@gmail.com>
2018-01-07 12:19:15 -0800

    Updated fmsData and field reference in field

--- src/org/usfirst/frc4904/robot/Robot.java
+++ src/org/usfirst/frc4904/robot/Robot.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc4904.robot;
 
+import org.usfirst.frc4904.autonly.Field;
 import org.usfirst.frc4904.standard.CommandRobotBase;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class Robot extends CommandRobotBase {
-
+	
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
@@ -25,7 +27,9 @@ public class Robot extends CommandRobotBase {
 	@Override
 	public void autonomousInitialize() {
 		// TODO Auto-generated method stub
-
+		String fmsData;
+		fmsData = DriverStation.getInstance().getGameSpecificMessage();
+		Field gamefield = new gameField(,fmsData)
 	}
 
 	@Override
