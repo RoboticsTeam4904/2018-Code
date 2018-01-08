3bbcee1f9cdb581621e2424ec9bab03bd4b1ae5b
Codadillo <leoconr@nuevaschool.org>
2018-01-07 16:14:23 -0800

    Updated defining of lift variables in robot map.

--- src/org/usfirst/frc4904/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc4904/robot/subsystems/Lifter.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc4904.robot.subsystems;
 
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lifter extends Subsystem {
