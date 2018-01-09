53260b24eff3bfb032d55b7dcc0b7bc86591d65e
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2018-01-08 17:07:16 -0800

    get and initialize motors

--- src/org/usfirst/frc4904/robot/subsystems/Arm.java
+++ src/org/usfirst/frc4904/robot/subsystems/Arm.java
@@ -5,7 +5,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Arm extends Subsystem{
 	
+	public final Motor elbowMotorA;
+	public final Motor elbowMotorB;
+	public final Motor wristMotorA;
+	public final Motor wristMotorB;
 	
+	public Arm (Motor elbowMotorA, Motor elbowMotorB, Motor wristMotorA, Motor wristMotorB) {
+		this.elbowMotorA = elbowMotorA;
+		this.elbowMotorB = elbowMotorB;
+		this.wristMotorA = wristMotorA;
+		this.wristMotorB = wristMotorB;
+	}
+
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub. Call the other command
