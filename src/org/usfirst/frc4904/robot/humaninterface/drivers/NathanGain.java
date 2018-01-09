cc6547bf4f56214f853ba721911813101bc961f4
billwpierce <wwpierce020602@gmail.com>
2018-01-08 17:17:18 -0800

    rerenamed to nathangain

--- src/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
+++ src/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
@@ -2,11 +2,9 @@ package org.usfirst.frc4904.robot.humaninterface.drivers;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.chassis.ChassisMove;
 import org.usfirst.frc4904.standard.humaninput.Driver;
-import edu.wpi.first.wpilibj.command.Command;
 
-public class SkrtSkrt extends Driver {
+public class NathanGain extends Driver {
 	public static final double SPEED_GAIN = 1;
 	public static final double SPEED_EXP = 2;
 	public static final double TURN_GAIN = 1;
@@ -17,8 +15,8 @@ public class SkrtSkrt extends Driver {
 	public static final double TURN_SPEED_SCALE = 1;
 	public static final double PASSIVE_CLIMBER_SPIN_SPEED = 0.07;
 
-	public SkrtSkrt() {
-		super("SkrtSkrt");
+	public NathanGain() {
+		super("NathanGain");
 	}
 
 	protected double scaleGain(double input, double gain, double exp) {
@@ -36,16 +34,16 @@ public class SkrtSkrt extends Driver {
 	@Override
 	public double getY() {
 		double rawSpeed = RobotMap.Component.driverXbox.rt.getX() - RobotMap.Component.driverXbox.lt.getX();
-		double speed = scaleGain(rawSpeed, SkrtSkrt.SPEED_GAIN, SkrtSkrt.SPEED_EXP)
-			* SkrtSkrt.Y_SPEED_SCALE;
+		double speed = scaleGain(rawSpeed, NathanGain.SPEED_GAIN, NathanGain.SPEED_EXP)
+			* NathanGain.Y_SPEED_SCALE;
 		return speed;
 	}
 
 	@Override
 	public double getTurnSpeed() {
 		double rawTurnSpeed = RobotMap.Component.driverXbox.leftStick.getX();
-		double turnSpeed = scaleGain(rawTurnSpeed, SkrtSkrt.TURN_GAIN, SkrtSkrt.TURN_EXP)
-			* SkrtSkrt.TURN_SPEED_SCALE;
+		double turnSpeed = scaleGain(rawTurnSpeed, NathanGain.TURN_GAIN, NathanGain.TURN_EXP)
+			* NathanGain.TURN_SPEED_SCALE;
 		return turnSpeed;
 	}
 }
