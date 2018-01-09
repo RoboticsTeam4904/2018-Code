d12bfd1fa246c2365ccba1c5e16a97288be04ece
billwpierce <wwpierce020602@gmail.com>
2018-01-08 17:09:29 -0800

    renamed driver class, removed useless command

--- src/org/usfirst/frc4904/robot/humaninterface/drivers/DefaultGain.java
+++ src/org/usfirst/frc4904/robot/humaninterface/drivers/DefaultGain.java
@@ -6,7 +6,7 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisMove;
 import org.usfirst.frc4904.standard.humaninput.Driver;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class SkrtSkrt extends Driver {
+public class DefaultGain extends Driver {
 	public static final double SPEED_GAIN = 1;
 	public static final double SPEED_EXP = 2;
 	public static final double TURN_GAIN = 1;
@@ -17,8 +17,8 @@ public class SkrtSkrt extends Driver {
 	public static final double TURN_SPEED_SCALE = 1;
 	public static final double PASSIVE_CLIMBER_SPIN_SPEED = 0.07;
 
-	public SkrtSkrt() {
-		super("NathanGain");
+	public DefaultGain() {
+		super("DefaultGain");
 	}
 
 	protected double scaleGain(double input, double gain, double exp) {
@@ -26,9 +26,7 @@ public class SkrtSkrt extends Driver {
 	}
 
 	@Override
-	public void bindCommands() {
-		Command normalDrive = new ChassisMove(RobotMap.Component.chassis, this);
-	}
+	public void bindCommands() {}
 
 	@Override
 	public double getX() {
@@ -38,16 +36,16 @@ public class SkrtSkrt extends Driver {
 	@Override
 	public double getY() {
 		double rawSpeed = RobotMap.Component.driverXbox.rt.getX() - RobotMap.Component.driverXbox.lt.getX();
-		double speed = scaleGain(rawSpeed, SkrtSkrt.SPEED_GAIN, SkrtSkrt.SPEED_EXP)
-			* SkrtSkrt.Y_SPEED_SCALE;
+		double speed = scaleGain(rawSpeed, DefaultGain.SPEED_GAIN, DefaultGain.SPEED_EXP)
+			* DefaultGain.Y_SPEED_SCALE;
 		return speed;
 	}
 
 	@Override
 	public double getTurnSpeed() {
 		double rawTurnSpeed = RobotMap.Component.driverXbox.leftStick.getX();
-		double turnSpeed = scaleGain(rawTurnSpeed, SkrtSkrt.TURN_GAIN, SkrtSkrt.TURN_EXP)
-			* SkrtSkrt.TURN_SPEED_SCALE;
+		double turnSpeed = scaleGain(rawTurnSpeed, DefaultGain.TURN_GAIN, DefaultGain.TURN_EXP)
+			* DefaultGain.TURN_SPEED_SCALE;
 		return turnSpeed;
 	}
 }
