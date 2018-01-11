9864c01e82cf5304faeed222fbab2251fd302d3b
Codadillo <leoconr@nuevaschool.org>
2018-01-10 17:37:50 -0800

    Added left and right subclasses of switch time.

--- src/org/usfirst/frc4904/autonly/LeftSwitchTime.java
+++ src/org/usfirst/frc4904/autonly/LeftSwitchTime.java
@@ -1,21 +1,9 @@
 package org.usfirst.frc4904.autonly;
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
-import org.usfirst.frc4904.standard.commands.Noop;
-import org.usfirst.frc4904.standard.commands.RunIf;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- *
- */
-public class LeftSwitchTime extends CommandGroup {
-	public static final double TIME_APPROACH_SWITCH = -1; // TODO: Replace with real time
-
-    public LeftSwitchTime() { //TODO: Add rest of code.
-        addSequential(new ChassisConstant(RobotMap.Component.chassis, 0, AutonConfig.DEAD_RECKON_DRIVE_SPEED, 0,
-			LeftSwitchTime.TIME_APPROACH_SWITCH));
-        addSequential(new RunIf(new Noop(), () -> RobotMap.gamefield.ourSwitch.left == RobotMap.gamefield.ourTeam)); //TODO: Replace with outtake.
-    }
-
+public class LeftSwitchTime extends SwitchTime {
+	public LeftSwitchTime () {
+		super(RobotMap.gamefield.ourSwitch.left);
+	}
 }
