168bfa04012bef3dc9b76bf5788c041ddcf930b1
Isabelle A <isaanca@nuevaschool.org>
2018-01-10 17:30:13 -0800

    Finished piston grab and rollout commands

--- src/org/usfirst/frc4904/robot/commands/PistonIntakeGrab.java
+++ src/org/usfirst/frc4904/robot/commands/PistonIntakeGrab.java
@@ -1,12 +1,18 @@
 package org.usfirst.frc4904.robot.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.subsytems.PistonIntake;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc4904.standard.commands.SingleOp;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 
 public class PistonIntakeGrab extends CommandGroup {
 	public PistonIntakeGrab() {
+		// Grabs a power cube by using the pistons and stopping the motors
+		
 		requires(RobotMap.Component.pistonIntake);
 		addParallel(new MotorConstant(RobotMap.Component.pistonIntake.roller, 0));
+		addParallel(new SingleOp(() -> RobotMap.Component.pistonIntake.piston.set(PistonIntake.GRABBED)));
 	}
+	
 }
--- src/org/usfirst/frc4904/robot/commands/PistonIntakeRollout.java
+++ src/org/usfirst/frc4904/robot/commands/PistonIntakeRollout.java
@@ -1,3 +1,18 @@
 package org.usfirst.frc4904.robot.commands;
 
-public class PistonIntakeRollout {}
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.subsytems.PistonIntake;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+
+public class PistonIntakeRollout extends CommandGroup {
+	public PistonIntakeRollout() {
+		// Makes the motors spin "outwards" to release a power cube
+		
+		requires(RobotMap.Component.pistonIntake);
+		addParallel(new MotorConstant(RobotMap.Component.pistonIntake.roller, PistonIntake.OUTTAKE_SPEED));
+		// Currently the motors will spin regardless of if piston is in the grabbed position
+		// Don't know if this will be a problem yet
+	}
+	
+}
