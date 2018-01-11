ca6a9850da96182d93039a00e7123e421104272d
Codadillo <leoconr@nuevaschool.org>
2018-01-10 17:38:23 -0800

    Fixed reversal of sides.

---
+++ src/org/usfirst/frc4904/autonly/RightSwitchTime.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc4904.autonly;
+
+import org.usfirst.frc4904.robot.RobotMap;
+
+public class RightSwitchTime extends SwitchTime {
+	public RightSwitchTime () {
+		super(RobotMap.gamefield.ourSwitch.right);
+	}
+}
