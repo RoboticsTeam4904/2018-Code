93b5a5ab41cd80860503581fc68795bc6ef6d027
Lauren W <lauwong@nuevaschool.org>
2018-01-13 19:58:25 -0800

    Git still isn't letting me checkout to master ... :(

--- src/org/usfirst/frc4904/robot/commands/ArmMove.java
+++ src/org/usfirst/frc4904/robot/commands/ArmMove.java
@@ -1,43 +1,36 @@
 package org.usfirst.frc4904.robot.commands;
 
+
 import org.usfirst.frc4904.robot.RobotMap;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class ArmMove extends CommandGroup{
-	
+public class ArmMove extends CommandGroup {
 	public ArmMove() {
 		requires(RobotMap.Component.boxio);
+	}
 
-	}
-	
 	@Override
-	protected void initialize() {
-		
-	}
-	
+	protected void initialize() {}
+
 	@Override
 	protected void execute() {
 		double leftStickY = RobotMap.HumanInput.Operator.leftStick.getY();
 		double rightStickY = RobotMap.HumanInput.Operator.rightStick.getY();
-		
 		RobotMap.Component.boxio.elbowMotorA.set(leftStickY);
-		RobotMap.Component.boxio.elbowMotorB.set(leftStickY*-1);//either *-1, or invert one motor in setup
-		//TODO: is there a less ghetto way of setting both motors? Theoretically the xbox input could change in between and mess stuff up
+		RobotMap.Component.boxio.elbowMotorB.set(leftStickY * -1);// either *-1, or invert one motor in setup
+		// TODO: is there a less ghetto way of setting both motors? Theoretically the xbox input could change in between and mess stuff up
 		RobotMap.Component.boxio.wristMotorA.set(rightStickY);
-		RobotMap.Component.boxio.wristMotorB.set(rightStickY*-1);
-		
+		RobotMap.Component.boxio.wristMotorB.set(rightStickY * -1);
 	}
-	
+
 	@Override
 	protected boolean isFinished() {
 		return false;
 	}
 
 	@Override
-	protected void end() {
-	}
+	protected void end() {}
 
 	@Override
-	protected void interrupted() {
-	}
+	protected void interrupted() {}
 }
--- src/org/usfirst/frc4904/robot/subsystems/Arm.java
+++ src/org/usfirst/frc4904/robot/subsystems/Arm.java
@@ -1,36 +1,31 @@
 package org.usfirst.frc4904.robot.subsystems;
 
+
+import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.commands.Idle;
 import org.usfirst.frc4904.standard.custom.sensors.CANTalonEncoder;
-import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Arm extends Subsystem{
-	
+public class Arm extends Subsystem {
 	public static final double ELBOW_MULTIPLIER = 1.0;
 	public static final double WRIST_MULTIPLIER = 1.0;
-	
 	public final Motor elbowMotorA;
 	public final Motor elbowMotorB;
 	public final Motor wristMotorA;
 	public final Motor wristMotorB;
-	
 	public final CANTalonEncoder elbowEncoderA;
 	public final CANTalonEncoder elbowEncoderB;
 	public final CANTalonEncoder wristEncoderA;
 	public final CANTalonEncoder wristEncoderB;
-	
-	
-	public Arm (Motor elbowMotorA, Motor elbowMotorB, Motor wristMotorA, Motor wristMotorB,
+
+	public Arm(Motor elbowMotorA, Motor elbowMotorB, Motor wristMotorA, Motor wristMotorB,
 		CANTalonEncoder elbowEncoderA, CANTalonEncoder elbowEncoderB, CANTalonEncoder wristEncoderA,
 		CANTalonEncoder wristEncoderB) {
-
 		this.elbowMotorA = elbowMotorA;
 		this.elbowMotorB = elbowMotorB;
 		this.wristMotorA = wristMotorA;
 		this.wristMotorB = wristMotorB;
-		
 		this.elbowEncoderA = elbowEncoderA;
 		this.elbowEncoderB = elbowEncoderB;
 		this.wristEncoderA = wristEncoderA;
@@ -42,6 +37,4 @@ public class Arm extends Subsystem{
 		// TODO Auto-generated method stub. Do whatever we want to do when we turn the robot on
 		setDefaultCommand(new Idle(RobotMap.Component.boxio));
 	}
-	
-	
 }
