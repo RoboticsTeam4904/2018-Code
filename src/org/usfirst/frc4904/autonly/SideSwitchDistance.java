af142a70b96b3365585c13fefaaab77134e2a0eb
billwpierce <wwpierce020602@gmail.com>
2018-01-15 10:02:50 -0800

    reference made static

--- src/org/usfirst/frc4904/autonly/SideSwitchDistance.java
+++ src/org/usfirst/frc4904/autonly/SideSwitchDistance.java
@@ -1,11 +1,7 @@
 package org.usfirst.frc4904.autonly;
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.Noop;
-import org.usfirst.frc4904.standard.commands.RunIf;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
@@ -17,6 +13,6 @@ public class SideSwitchDistance extends Strategy {
 
 	@Override
 	public void setup() {
-		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * this.DISTANCE_APPROACH_SWITCH, RobotMap.Component.drivePID));
+		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_SWITCH, RobotMap.Component.drivePID));
     }
 }
