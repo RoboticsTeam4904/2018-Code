04cf4f744c1300c09f098c9af9b32d65a1003d69
billwpierce <wwpierce020602@gmail.com>
2018-01-15 10:09:48 -0800

    fixed if/else error from commandgroup

--- src/org/usfirst/frc4904/autonly/RightSideDistance.java
+++ src/org/usfirst/frc4904/autonly/RightSideDistance.java
@@ -1,18 +1,14 @@
 package org.usfirst.frc4904.autonly;
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.Noop;
+import org.usfirst.frc4904.standard.LogKitten;
+import org.usfirst.frc4904.standard.commands.KittenCommand;
+import org.usfirst.frc4904.standard.commands.RunIf;
 
 public class RightSideDistance extends SideSwitchDistance {
 	public RightSideDistance () {
-		super();
-	}
-	
-	@Override
-	public void initialize() { //TODO: Replace with actual deployment code.
-		super.initialize();
-		if(RobotMap.gameField.ourSwitch.right == RobotMap.gameField.team){
-			addSequential(new Noop());
-		}
+		addSequential(new RunIf(new KittenCommand("It's our right switch.", LogKitten.LEVEL_WTF), () -> {
+    		return RobotMap.gameField.ourSwitch.right == RobotMap.gameField.team;
+    	}));
 	}
 }
