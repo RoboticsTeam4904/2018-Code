bda4ffff486dd77c9437d7ed0f90131ebfe0eacc
billwpierce <wwpierce020602@gmail.com>
2018-01-15 10:14:30 -0800

    reordered sideswitchdistance

--- src/org/usfirst/frc4904/autonly/SideSwitchDistance.java
+++ src/org/usfirst/frc4904/autonly/SideSwitchDistance.java
@@ -9,10 +9,10 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 public class SideSwitchDistance extends Strategy {
 	public static final double DISTANCE_APPROACH_SWITCH = 9; // TODO: Test this distance.
 	
-	public SideSwitchDistance() {}
+	public SideSwitchDistance() {
+		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_SWITCH, RobotMap.Component.drivePID));
+	}
 
 	@Override
-	public void setup() {
-		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_SWITCH, RobotMap.Component.drivePID));
-    }
+	public void setup() {}
 }
