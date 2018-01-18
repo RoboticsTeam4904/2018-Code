11ae70a0235fdac9c2f210bfcaa3943e0814a4e6
Howard Stark <howard@getcoffee.io>
2018-01-17 16:16:20 -0800

    Fixed RobotMap initialization

--- src/org/usfirst/frc4904/robot/Robot.java
+++ src/org/usfirst/frc4904/robot/Robot.java
@@ -1,67 +1,58 @@
 package org.usfirst.frc4904.robot;
 
+
 import org.usfirst.frc4904.standard.CommandRobotBase;
 
 public class Robot extends CommandRobotBase {
+	private RobotMap robotMap = new RobotMap();
 
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void teleopInitialize() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void teleopExecute() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void autonomousInitialize() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void autonomousExecute() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void disabledInitialize() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void disabledExecute() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void testInitialize() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void testExecute() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void alwaysExecute() {
 		// TODO Auto-generated method stub
-
 	}
-
 }
