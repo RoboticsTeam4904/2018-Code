bf523ca940b95b877dda6e0178924fc3fc9a31b6
billwpierce <wwpierce020602@gmail.com>
2018-01-17 17:02:34 -0800

    Removed unused imports for no other reason than my sanity

--- src/org/usfirst/frc4904/autonly/LeftSideDistance.java
+++ src/org/usfirst/frc4904/autonly/LeftSideDistance.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc4904.autonly;
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.LogKitten;
-import org.usfirst.frc4904.standard.commands.KittenCommand;
 import org.usfirst.frc4904.standard.commands.Noop;
 import org.usfirst.frc4904.standard.commands.RunIf;
 
