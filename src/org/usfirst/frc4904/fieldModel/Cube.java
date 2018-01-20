64326f68c9b820e1f4cb83893da5dd03ca7fc0cb
Codadillo <leoconr@nuevaschool.org>
2018-01-19 18:28:38 -0800

    Expanded cube class.

--- src/org/usfirst/frc4904/fieldModel/Cube.java
+++ src/org/usfirst/frc4904/fieldModel/Cube.java
@@ -5,10 +5,31 @@ public class Cube {
 	//position of the cube
 	double x;
 	double y;
-	
 	//how likely it is that we actually saw a cube
-	int probability;
-	
+	int confidence;
 	//when we last saw the cube
-	int timeSeen;
+	int frameSeen;
+	
+	public Cube(double x, double y, int confidence) {
+		this.x = x;
+		this.y = y;
+		this.confidence = confidence;
+		frameSeen = AligningCamera.getFrame(); //TODO: merge branch vision-tables (it has AligningCamera)
+	}
+	
+	public int getAge() {
+		return AligningCamera.getFrame() - frameSeen;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public int getConfidence() {
+		return confidence;
+	}
 }
