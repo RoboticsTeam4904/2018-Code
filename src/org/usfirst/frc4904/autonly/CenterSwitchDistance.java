74d82aea9032411ff5498093a8ccaccd99d23182
billwpierce <wwpierce020602@gmail.com>
2018-01-22 17:02:16 -0800

    constant changes

--- src/org/usfirst/frc4904/autonly/CenterSwitchDistance.java
+++ src/org/usfirst/frc4904/autonly/CenterSwitchDistance.java
@@ -9,17 +9,16 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisTurn;
 public class CenterSwitchDistance extends Strategy {
 	public static final double DISTANCE_APPROACH_MID = -1; // TODO: real values
 	public static final double DISTANCE_MID_SWITCH = -1;
-	public static final double DEGREES_TURN_LEFT = -1;
-	public static final double DEGREES_TURN_RIGHT = -1;
+	public static final double DEGREES_TURN = -1;
 
 	public CenterSwitchDistance() {
 		addSequential(
 			new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_MID,
 				RobotMap.Component.drivePID));
 		addSequential(new RunIfElse(
-			new ChassisTurn(RobotMap.Component.chassis, DEGREES_TURN_LEFT, RobotMap.Component.navx,
+			new ChassisTurn(RobotMap.Component.chassis, -1 * DEGREES_TURN, RobotMap.Component.navx,
 				RobotMap.Component.chassisTurnMC),
-			new ChassisTurn(RobotMap.Component.chassis, DEGREES_TURN_RIGHT, RobotMap.Component.navx,
+			new ChassisTurn(RobotMap.Component.chassis, DEGREES_TURN, RobotMap.Component.navx,
 				RobotMap.Component.chassisTurnMC),
 			() -> {
 				return RobotMap.gameField.ourSwitch.isLeftOurs();
@@ -27,11 +26,11 @@ public class CenterSwitchDistance extends Strategy {
 		addSequential(
 			new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_MID_SWITCH,
 				RobotMap.Component.drivePID));
-		//The following orients the robot to be perpendicular with the switch again. May not be necessary
+		// The following orients the robot to be perpendicular with the switch again. May not be necessary
 		addSequential(new RunIfElse(
-			new ChassisTurn(RobotMap.Component.chassis, -DEGREES_TURN_LEFT, RobotMap.Component.navx,
+			new ChassisTurn(RobotMap.Component.chassis, DEGREES_TURN, RobotMap.Component.navx,
 				RobotMap.Component.chassisTurnMC),
-			new ChassisTurn(RobotMap.Component.chassis, -DEGREES_TURN_RIGHT, RobotMap.Component.navx,
+			new ChassisTurn(RobotMap.Component.chassis, -1 * DEGREES_TURN, RobotMap.Component.navx,
 				RobotMap.Component.chassisTurnMC),
 			() -> {
 				return RobotMap.gameField.ourSwitch.isLeftOurs();
--- src/org/usfirst/frc4904/autonly/SideSwitchDistance.java
+++ src/org/usfirst/frc4904/autonly/SideSwitchDistance.java
@@ -1,13 +1,15 @@
 package org.usfirst.frc4904.autonly;
 
+
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 
 public class SideSwitchDistance extends Strategy {
-	public static final double DISTANCE_APPROACH_SWITCH = 9; // TODO: Test this distance.
-	
+	public static final double DISTANCE_APPROACH_SWITCH = 9 * 12; // TODO: Test this distance.
+
 	public SideSwitchDistance() {
-		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_SWITCH, RobotMap.Component.drivePID));
+		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
+			RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_SWITCH, RobotMap.Component.drivePID));
 	}
 
 	@Override
