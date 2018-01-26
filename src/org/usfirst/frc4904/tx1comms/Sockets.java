d2bba1bd3c3f94dde5ccbce917f2ef6f0bf7afdb
Codadillo <leoconr@nuevaschool.org>
2018-01-26 09:00:16 -0800

    Changed printwriter to outputstream.

--- src/org/usfirst/frc4904/tx1comms/Sockets.java
+++ src/org/usfirst/frc4904/tx1comms/Sockets.java
@@ -3,10 +3,12 @@ package org.usfirst.frc4904.tx1comms;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Sockets {
 	
@@ -15,7 +17,7 @@ public class Sockets {
 	
 	public ServerSocket listener;
 	BufferedReader in;
-	PrintWriter out;
+	OutputStreamWriter out;
 	
 	public Sockets() {
 		try {
@@ -23,15 +25,18 @@ public class Sockets {
 			Socket sock = listener.accept();
 			in = new BufferedReader(new InputStreamReader(
 				sock.getInputStream()));
-			out = new PrintWriter(sock.getOutputStream(), true);
-			
+			out = new OutputStreamWriter(sock.getOutputStream(), StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public void sendMessage(String message) {
-		out.println(message);
-		out.flush();
+		try {
+			out.write(message);
+			out.flush();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
