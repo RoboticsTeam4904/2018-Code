82f7984fd4ab04b3c8d752b0ac3998e82cf8daca
Evan Steirman <evastei@nuevaschool.org>
2018-01-26 13:06:08 -0800

    Added documentation to RollersIntake, RollersOuttake, and CrateIO.

--- src/org/usfirst/frc4904/robot/subsystems/CrateIO.java
+++ src/org/usfirst/frc4904/robot/subsystems/CrateIO.java
@@ -1,14 +1,29 @@
 package org.usfirst.frc4904.robot.subsystems;
 
+
 import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class CrateIO extends Subsystem {	
+/**
+ * CrateIO is the subsystem of rollers
+ * in the front of the robot, allowing
+ * it to intake/outtake cubes in front
+ * of it.
+ */
+public class CrateIO extends Subsystem {
 	public static final double INTAKE_SPEED = 0.5;
 	public static final double OUTTAKE_SPEED = -0.5;
 	public Motor roller;
-	
+
+	/**
+	 * Create a new CrateIO instance.
+	 * 
+	 * @param roller
+	 *        The motor object corresponding
+	 *        to the motors for intaking
+	 *        and outtaking cubes.
+	 */
 	public CrateIO(Motor roller) {
 		this.roller = roller;
 	}
@@ -21,5 +36,4 @@ public class CrateIO extends Subsystem {
 	public void set(double speed) {
 		roller.set(speed);
 	}
-
 }
