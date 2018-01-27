d9358eb031de89ec3beaaadccc0784714e3aa761
CaseyManning <kcgrind@gmail.com>
2018-01-27 11:12:50 -0800

    changed so we can switch between usb and sockets

---
+++ src/org/usfirst/frc4904/tx1comms/TX1Comms.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc4904.tx1comms;
+
+import java.util.HashMap;
+import org.json.JSONObject;
+
+public class TX1Comms {
+	
+	public static final boolean usingSockets = true;
+	
+	public String getEncoderData() {
+		HashMap<String, Integer> encoders = new HashMap<String, Integer>();	
+//		encoders.put("leftBack", ((Encoder) null).get());
+//		encoders.put("leftFront", ((Encoder) null).get());
+//		encoders.put("rightBack", ((Encoder) null).get());
+//		encoders.put("rightFront", ((Encoder) null).get());
+		encoders.put("leftBack", 5);
+		encoders.put("leftFront", 4);
+		encoders.put("rightBack", 3);
+		encoders.put("rightFront", 2);
+
+		String message = JSONObject.valueToString(encoders);
+		//sendMessage(message);
+		return message;
+	}
+	
+	
+	public String getIMUData() {
+		//IMU imu =  new IMU(); //RobotMap.Component.IMU (but it doesn't exist yet)
+		
+		String message = JSONObject.valueToString(5);
+		return message;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("sending data");
+		Sockets s = new Sockets();
+		YewEsBee usb = new YewEsBee();
+		while(true) {
+			System.out.println("sending data");
+			try {
+				Thread.sleep(20);
+			}
+			catch (InterruptedException e) {e.printStackTrace();}
+			if(usingSockets) {
+				s.sendData();
+			} else {
+				
+			}
+		}
+	}
+}
