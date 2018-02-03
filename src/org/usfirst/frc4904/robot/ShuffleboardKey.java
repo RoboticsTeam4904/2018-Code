966b32a42d2934019885a9f751c77934f5f0b993
Isabelle A <isaanca@nuevaschool.org>
2018-02-02 16:14:22 -0800

    arbitrary 2.0

---
+++ src/org/usfirst/frc4904/robot/ShuffleboardKey.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc4904.robot;
+
+public enum ShuffleboardKey {
+	TEMPORARY("this is a filler until we have something better to put here");
+	
+	public final String key;
+	private ShuffleboardKey(String key) {
+		this.key = key;
+	}
+}
