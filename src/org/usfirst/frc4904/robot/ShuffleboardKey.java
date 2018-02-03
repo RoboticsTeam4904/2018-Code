2289d881d3544bcc417c3fcf99527cd938814c16
Lauren W <lauwong@nuevaschool.org>
2018-02-02 16:52:17 -0800

    Added 2017-Code Smart Dashboard enum contents to ShuffleboardKey.java in 2018-Code

--- src/org/usfirst/frc4904/robot/ShuffleboardKey.java
+++ src/org/usfirst/frc4904/robot/ShuffleboardKey.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc4904.robot;
 
+
 public enum ShuffleboardKey {
-	TEMPORARY("this is a filler until we have something better to put here");
-	
+	SUBSYSTEM_SUMMARY("Subsystem summary"), VISION_AUTOCALIBRATION("Autocalibration"), VISION_AUTOCALIBRATION_COMPLETE(
+		"Autocalibration complete");
 	public final String key;
+
 	private ShuffleboardKey(String key) {
 		this.key = key;
 	}
