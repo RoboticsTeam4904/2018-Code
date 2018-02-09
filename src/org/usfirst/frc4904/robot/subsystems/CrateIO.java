114d3491c63b493d591df3d2516c57c76dbcecef
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2018-02-08 17:27:57 -0800

    rararasputin

--- src/org/usfirst/frc4904/robot/subsystems/CrateIO.java
+++ src/org/usfirst/frc4904/robot/subsystems/CrateIO.java
@@ -7,19 +7,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class CrateIO extends Subsystem {	
 	public static final double INTAKE_SPEED = 0.5;
 	public static final double OUTTAKE_SPEED = -0.5;
-	public Motor roller;
+	public Motor rollerLeft;
+	public Motor rollerRight;
 	
-	public CrateIO(Motor roller) {
-		this.roller = roller;
+	public CrateIO(Motor rollerLeft, Motor rollerRight) {
+		this.rollerLeft = rollerLeft;
+		this.rollerRight = rollerRight;
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		setDefaultCommand(new MotorIdle(roller));
+//		setDefaultCommand(new MotorIdle(roller));
 	}
 
 	public void set(double speed) {
-		roller.set(speed);
+		rollerLeft.set(speed);
+		rollerRight.set(speed);
 	}
 
 }
