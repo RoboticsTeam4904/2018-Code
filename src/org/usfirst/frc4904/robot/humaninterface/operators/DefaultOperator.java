7ac38ba529212bdfa740dc671e872b1e1fd572ad
Osher Lerner <osherler@gmail.com>
2018-02-08 23:00:16 -0800

    remapped buttons for operator

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -8,6 +8,11 @@ import org.usfirst.frc4904.robot.commands.IntakeRollersOuttake;
 import org.usfirst.frc4904.standard.humaninput.Operator;
 
 public class DefaultOperator extends Operator {
+
+	public DefaultOperator() {
+		super("DefaultOperator");
+	}
+
 	public DefaultOperator(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
@@ -15,9 +20,9 @@ public class DefaultOperator extends Operator {
 
 	@Override
 	public void bindCommands() {
-		RobotMap.Component.joystick.button1.onlyWhileHeld(new IndexerRollersIntake());
-		RobotMap.Component.joystick.button2.onlyWhileHeld(new IndexerRollersOuttake());
-		RobotMap.Component.joystick.button3.onlyWhileHeld(new IntakeRollersIntake());
-		RobotMap.Component.joystick.button4.onlyWhileHeld(new IntakeRollersOuttake());
+		RobotMap.Component.joystick.button5.onlyWhileHeld(new IntakeRollersIntake());
+		RobotMap.Component.joystick.button6.onlyWhileHeld(new IntakeRollersOuttake());
+		RobotMap.Component.joystick.button3.onlyWhileHeld(new IndexerRollersIntake());
+		RobotMap.Component.joystick.button4.onlyWhileHeld(new IndexerRollersOuttake());
 	}
 }
