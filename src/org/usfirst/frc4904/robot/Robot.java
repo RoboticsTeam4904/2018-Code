6e92875a79db7faf2b2853632b4655d450a5dc94
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2018-02-11 14:11:32 -0800

    removed autogenerated stubs

--- src/org/usfirst/frc4904/robot/Robot.java
+++ src/org/usfirst/frc4904/robot/Robot.java
@@ -23,41 +23,33 @@ public class Robot extends CommandRobotBase {
 
 	@Override
 	public void teleopExecute() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void autonomousInitialize() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void autonomousExecute() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void disabledInitialize() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void disabledExecute() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void testInitialize() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void testExecute() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void alwaysExecute() {
-		// TODO Auto-generated method stub
 	}
 }
