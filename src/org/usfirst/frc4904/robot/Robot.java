954c00b08829f826239aa600924a625ca3c07763
Isabelle A <isaanca@nuevaschool.org>
2018-02-11 11:25:55 -0800

    add arm angle and auton routine selector to shuffleboard

--- src/org/usfirst/frc4904/robot/Robot.java
+++ src/org/usfirst/frc4904/robot/Robot.java
@@ -4,13 +4,16 @@ package org.usfirst.frc4904.robot;
 import org.usfirst.frc4904.standard.CommandRobotBase;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc4904.robot.subsystems.arm;
 
 public class Robot extends CommandRobotBase {
 	private RobotMap robotMap = new RobotMap();
 
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
+		autoChooser.addDefault(CenterSwitchDistance());
+		autoChooser.addDefault(LeftSideDistance());
+		autoChooser.addDefault(RightSideDistance());
 	}
 
 	@Override
@@ -56,14 +59,15 @@ public class Robot extends CommandRobotBase {
 	@Override
 	public void alwaysExecute() {
 		putSBSubsystemSummary();
+		SmartDashboard.putNumber("Arm Angle", arm.getAngle());
 		// TODO Auto-generated method stub
 	}
-	
+
 	void putSBSubsystemSummary() {
 		String summary = "";
 		for (Subsystem subsystem : RobotMap.Component.mainSubsystems) {
 			summary += "{" + subsystem.getName() + "} running command {" + subsystem.getCurrentCommand() + "}\n";
 		}
-		SmartDashboard.putString("Subsystem summary", summary);
+		SmartDashboard.putString("Subsystem Overview", summary);
 	}
 }
