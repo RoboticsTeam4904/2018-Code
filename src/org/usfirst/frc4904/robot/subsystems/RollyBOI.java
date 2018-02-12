2696746a253bafb4bc5583a0c354d9cf674088d7
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2018-02-11 19:55:44 -0800

    some small changes to default command

--- src/org/usfirst/frc4904/robot/subsystems/RollyBOI.java
+++ src/org/usfirst/frc4904/robot/subsystems/RollyBOI.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc4904.robot.subsystems;
 
 
-import org.usfirst.frc4904.robot.commands.IndexerGrabberRelease;
+import org.usfirst.frc4904.robot.commands.IndexerGrabberClasp;
 import org.usfirst.frc4904.robot.commands.MotorIdleGroup;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -47,7 +47,7 @@ public class RollyBOI extends Subsystem {
 
 		@Override
 		protected void initDefaultCommand() {
-			setDefaultCommand(new IndexerGrabberRelease());
+			setDefaultCommand(new IndexerGrabberClasp());
 		}
 	}
 }
