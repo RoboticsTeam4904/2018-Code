6377681aaf74d543d0084fe811ef56f60c192d50
billwpierce <wwpierce020602@gmail.com>
2018-02-20 12:15:37 -0800

    made variable names capitalized

--- src/org/usfirst/frc4904/robot/commands/ReleaseIntake.java
+++ src/org/usfirst/frc4904/robot/commands/ReleaseIntake.java
@@ -8,15 +8,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class ReleaseIntake extends CommandGroup {
-	private static final double releaseSpeed = 0.3;
-	private static final double releaseTime = 0.5;
-	private static final double waitTime = 0.25;
+	private static final double RELEASE_SPEED = 0.3;
+	private static final double RELEASE_TIME = 0.5;
+	private static final double WAIT_TIME = 0.25;
 
 	public ReleaseIntake() {
 		addSequential(
-			new RunFor(new MotorConstant(RobotMap.Component.crateIORollerRight, releaseSpeed), releaseTime));
-		addSequential(new WaitCommand(waitTime));
+			new RunFor(new MotorConstant(RobotMap.Component.crateIORollerRight, RELEASE_SPEED), RELEASE_TIME));
+		addSequential(new WaitCommand(WAIT_TIME));
 		addSequential(
-			new RunFor(new MotorConstant(RobotMap.Component.crateIORollerLeft, releaseSpeed), releaseTime));
+			new RunFor(new MotorConstant(RobotMap.Component.crateIORollerLeft, RELEASE_SPEED), RELEASE_TIME));
 	}
 }
