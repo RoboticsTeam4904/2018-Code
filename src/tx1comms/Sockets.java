668766b10d61689e1057b24c84ba0bbfd365c357
CaseyManning <kcgrind@gmail.com>
2018-02-24 16:01:27 -0800

    added sockets code

---
+++ src/tx1comms/Sockets.java
@@ -0,0 +1,66 @@
+package tx1comms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import org.json.JSONObject;
+
+public class Sockets {
+	
+	public static int PORT_NUMBER = 5202;
+	public static final String HOSTNAME = "127.0.0.1"; //"tegra-ubuntu.local";
+	
+	public ServerSocket listener;
+	BufferedReader in;
+	PrintWriter out;
+	
+	public Sockets() {
+		try {
+			listener = new ServerSocket(PORT_NUMBER, 100, InetAddress.getByName(HOSTNAME));
+			Socket sock = listener.accept();
+			in = new BufferedReader(new InputStreamReader(
+				sock.getInputStream()));
+			out = new PrintWriter(sock.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(String message) {
+		out.println(message);
+		out.flush();
+	}
+	
+	public void sendData(String message) {
+		sendMessage(message);
+	}
+	
+	public String getEncoderData() {
+		HashMap<String, Integer> encoders = new HashMap<String, Integer>();	
+//		encoders.put("leftBack", ((Encoder) null).get());
+//		encoders.put("leftFront", ((Encoder) null).get());
+//		encoders.put("rightBack", ((Encoder) null).get());
+//		encoders.put("rightFront", ((Encoder) null).get());
+		encoders.put("leftBack", 5);
+		encoders.put("leftFront", 4);
+		encoders.put("rightBack", 3);
+		encoders.put("rightFront", 2);
+
+		String message = JSONObject.valueToString(encoders);
+		//sendMessage(message);
+		return message;
+	}
+	
+	
+	public String getIMUData() {
+		//IMU imu =  new IMU(); //RobotMap.Component.IMU (but it doesn't exist yet)
+		
+		String message = JSONObject.valueToString(5);
+		return message;
+	}
+}
---
+++ src/tx1comms/TX1Comms.java
@@ -0,0 +1,59 @@
+package tx1comms;
+
+import java.util.HashMap;
+import org.json.JSONObject;
+import org.usfirst.frc4904.standard.LogKitten;
+
+public class TX1Comms implements Runnable{
+	
+	
+	//The encoders don't exist yet, so we are pretending we can get the data
+	public static String getEncoderData() {
+		HashMap<String, Integer> encoders = new HashMap<String, Integer>();	
+//		encoders.put("leftBack", ((Encoder) RobotMap.whatever).get());
+//		encoders.put("leftFront", ((Encoder) RobotMap.whatever).get());
+//		encoders.put("rightBack", ((Encoder) RobotMap.whatever).get());
+//		encoders.put("rightFront", ((Encoder) RobotMap.whatever).get());
+		encoders.put("leftBack", 5);
+		encoders.put("leftFront", 4);
+		encoders.put("rightBack", 3);
+		encoders.put("rightFront", 2);
+
+		String message = JSONObject.valueToString(encoders);
+		//sendMessage(message);
+		return message;
+	}
+	
+	//The IMU doesn't exist yet, so we are pretending we can get the data
+	public static String getIMUData() {
+		//IMU imu =  new IMU(); //RobotMap.Component.IMU (but it doesn't exist yet)
+		
+		String message = JSONObject.valueToString(5);
+		return message;
+	}
+	
+	//only for testing purposes
+	public static void main(String[] args) {
+		System.out.println("starting");
+		new TX1Comms().run();
+	}
+
+	@Override
+	public void run() {
+		Sockets s = new Sockets();
+		System.out.println("connection established");
+		while(true) {
+			System.out.println("sending data");
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			String message = "{ \"encoders:\" " + getEncoderData() + ", \"IMU\": " + getIMUData() + "}" + " " + System.currentTimeMillis();
+			s.sendData(message);
+
+		}
+
+	}
+}
