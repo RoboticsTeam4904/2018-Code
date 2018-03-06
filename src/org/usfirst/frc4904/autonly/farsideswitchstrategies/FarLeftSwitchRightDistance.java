8c9928dc99744b1424007281d9a883afaf84b6e5
Lauren W <lauwong@nuevaschool.org>
2018-03-05 16:51:48 -0800

    Commented switch strategies and added far left scale strategies + far side scale strategy

--- src/org/usfirst/frc4904/autonly/farsideswitchstrategies/FarLeftSwitchRightDistance.java
+++ src/org/usfirst/frc4904/autonly/farsideswitchstrategies/FarLeftSwitchRightDistance.java
@@ -2,31 +2,41 @@ package org.usfirst.frc4904.autonly.farsideswitchstrategies;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.commands.ArmSet;
 import org.usfirst.frc4904.robot.commands.OuttakeSquared;
+import org.usfirst.frc4904.robot.subsystems.Arm;
 import org.usfirst.frc4904.standard.commands.RunIf;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisTurn;
 
 public class FarLeftSwitchRightDistance extends FarSideSwitchStrategy {
 	public FarLeftSwitchRightDistance() {
+		// Move forwards past switch
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
 			RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_TO_BEHIND_SWITCH, RobotMap.Component.drivePID));
+		// Turn right
 		addSequential(
 			new ChassisTurn(RobotMap.Component.chassis, -90, RobotMap.Component.navx, RobotMap.Component.chassisTurnMC));
+		// Cross behind switch
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
 			RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_CROSS_BEHIND_SWITCH, RobotMap.Component.drivePID));
+		// Turn right
 		addSequential(
 			new ChassisTurn(RobotMap.Component.chassis, -90, RobotMap.Component.navx, RobotMap.Component.chassisTurnMC));
-		// addSequential(new RunIf(new ArmSet(Arm.ArmState.ARM_POSITION_SWITCH), () -> {
-		// return RobotMap.gameField.ourSwitch.isLeftOurs();
-		// }));
+		// Lift arm to switch height
+		addSequential(new RunIf(new ArmSet(Arm.ArmState.ARM_POSITION_SWITCH), () -> {
+			return RobotMap.gameField.ourSwitch.isRightOurs();
+		}));
+		// Approach switch
 		addSequential(new RunIf(new ChassisMoveDistance(RobotMap.Component.chassis,
 			RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_FAR_SWITCH, RobotMap.Component.drivePID), () -> {
 				return RobotMap.gameField.ourSwitch.isRightOurs();
 			}));
+		// Outtake cube
 		addSequential(new RunIf(new OuttakeSquared(), () -> {
 			return RobotMap.gameField.ourSwitch.isRightOurs();
 		}));
+		// Back away from switch
 		addSequential(new RunIf(new ChassisMoveDistance(RobotMap.Component.chassis,
 			-RobotMap.Metrics.Wheel.TICKS_PER_INCH * DISTANCE_APPROACH_FAR_SWITCH, RobotMap.Component.drivePID), () -> {
 				return RobotMap.gameField.ourSwitch.isRightOurs();
