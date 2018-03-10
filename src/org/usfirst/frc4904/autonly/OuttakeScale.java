67dfd7c5b534a5b14f3e3857f16951b3a26c60d5
Evan Steirman <evastei@nuevaschool.org>
2018-03-09 17:28:48 -0800

    shiley's constant has been created. also, the robot now moves 6 inches more (specifically 6 inches).

--- src/org/usfirst/frc4904/autonly/OuttakeScale.java
+++ src/org/usfirst/frc4904/autonly/OuttakeScale.java
@@ -13,22 +13,26 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisMoveDistance;
  *
  */
 public class OuttakeScale extends Strategy {
+	protected static final double APPROACH_DISTANCE_PAST_SCALE = 6; // this shall now be known as Shiley's Constant.
+
 	/**
 	 * Constructor. Never call unless certain outtaking onto OUR scale.
 	 *
 	 * @param approach_dist
 	 *        Positive distance to approach the scale. Will drive backwards.
 	 */
-	public OuttakeScale(double approach_dist) { // TODO: take into account relative position to scale when outtttttttaking
+	public OuttakeScale(double approach_dist) {
 		// Set arm to scale position
 		addSequential(new ArmSet(Arm.ArmState.ARM_POSITION_SCALE));
 		// Back up towards scale
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
-			-RobotMap.Metrics.Wheel.TICKS_PER_INCH * approach_dist, RobotMap.Component.drivePID));
+			-RobotMap.Metrics.Wheel.TICKS_PER_INCH * (approach_dist + APPROACH_DISTANCE_PAST_SCALE),
+			RobotMap.Component.drivePID));
 		// Outtake cube
 		addSequential(new OuttakeSquared());
 		// Move forwards away from scale
-		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, RobotMap.Metrics.Wheel.TICKS_PER_INCH * approach_dist,
+		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis,
+			RobotMap.Metrics.Wheel.TICKS_PER_INCH * (approach_dist + APPROACH_DISTANCE_PAST_SCALE),
 			RobotMap.Component.drivePID));
 		// Set arm to intake position
 		addSequential(new ArmSet(Arm.ArmState.ARM_POSITION_INTAKE));
