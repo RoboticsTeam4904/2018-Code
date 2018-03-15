9010b0e5d85329855fb16fac8b429ae070d3dd1c
Evan Steirman <evastei@nuevaschool.org>
2018-03-15 16:39:29 -0700

    now the arm has a default position after it intakes, but it *should* go back to normal height for intaking new cubes

--- src/org/usfirst/frc4904/robot/subsystems/Arm.java
+++ src/org/usfirst/frc4904/robot/subsystems/Arm.java
@@ -22,7 +22,7 @@ public class Arm extends PositionSensorMotor {
 		ArmState.ARM_POSITION_SCALE.position);
 
 	public enum ArmState {
-		ARM_POSITION_INTAKE(0), ARM_POSITION_SWITCH(53), ARM_POSITION_SCALE(143); // TODO: need to be tweaked a lot
+		ARM_POSITION_INTAKE(0), ARM_POSITION_INTOOK(5), ARM_POSITION_SWITCH(53), ARM_POSITION_SCALE(143); // TODO: need to be tweaked a lot
 		public final double position;
 
 		private ArmState(double position) {
