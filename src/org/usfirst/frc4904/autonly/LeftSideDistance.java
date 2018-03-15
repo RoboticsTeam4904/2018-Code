6b728ae7a50c3a2403dc358b74670fd351d8e615
Lauren W <lauwong@nuevaschool.org>
2018-03-15 16:47:32 -0700

    Removed SideSwitchDistance and rewrote child SideDistances

--- src/org/usfirst/frc4904/autonly/LeftSideDistance.java
+++ src/org/usfirst/frc4904/autonly/LeftSideDistance.java
@@ -2,18 +2,23 @@ package org.usfirst.frc4904.autonly;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.commands.IndexerOuttake;
 import org.usfirst.frc4904.standard.LogKitten.KittenLevel;
 import org.usfirst.frc4904.standard.commands.KittenCommand;
-import org.usfirst.frc4904.standard.commands.RunIf;
+import org.usfirst.frc4904.standard.commands.RunIfElse;
 
-public class LeftSideDistance extends SideSwitchDistance {
+public class LeftSideDistance extends Strategy {
+	public static final double DISTANCE_APPROACH_SWITCH = 144 - RobotMap.Metrics.LENGTH;
+
 	public LeftSideDistance() {
 		super();
 		addSequential(new KittenCommand(
 			"Is the left switch ours: " + Boolean.toString(RobotMap.gameField.ourSwitch.isLeftOurs()), KittenLevel.WTF));
-		addSequential(new RunIf(new IndexerOuttake(AutonConfig.AUTON_OUTTAKE_SPEED), () -> {
-			return RobotMap.gameField.ourSwitch.isLeftOurs();
-		}));
+		addSequential(new RunIfElse(new OuttakeSwitch(DISTANCE_APPROACH_SWITCH),
+			new CrossBaseline(DISTANCE_APPROACH_SWITCH), () -> {
+				return RobotMap.gameField.ourSwitch.isLeftOurs();
+			}));
 	}
+
+	@Override
+	public void setup() {}
 }
--- src/org/usfirst/frc4904/autonly/RightSideDistance.java
+++ src/org/usfirst/frc4904/autonly/RightSideDistance.java
@@ -2,14 +2,19 @@ package org.usfirst.frc4904.autonly;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.Noop;
-import org.usfirst.frc4904.standard.commands.RunIf;
+import org.usfirst.frc4904.standard.commands.RunIfElse;
 
-public class RightSideDistance extends SideSwitchDistance {
+public class RightSideDistance extends Strategy {
+	public static final double DISTANCE_APPROACH_SWITCH = 144 - RobotMap.Metrics.LENGTH;
+
 	public RightSideDistance() {
 		super();
-		addSequential(new RunIf(new Noop(), () -> {
-			return RobotMap.gameField.ourSwitch.isRightOurs();
-		}));
+		addSequential(new RunIfElse(new OuttakeSwitch(DISTANCE_APPROACH_SWITCH),
+			new CrossBaseline(DISTANCE_APPROACH_SWITCH), () -> {
+				return RobotMap.gameField.ourSwitch.isRightOurs();
+			}));
 	}
+
+	@Override
+	public void setup() {}
 }
