27f85a0e702d1d4e52b5a92173f9408a331e3cd1
Osher Lerner <osherler@gmail.com>
2018-03-17 14:29:12 -0700

    fixed RightSideTime

--- src/org/usfirst/frc4904/autonly/RightSideTime.java
+++ src/org/usfirst/frc4904/autonly/RightSideTime.java
@@ -2,14 +2,20 @@ package org.usfirst.frc4904.autonly;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.Noop;
+import org.usfirst.frc4904.robot.commands.IndexerOuttake;
+import org.usfirst.frc4904.standard.LogKitten.KittenLevel;
+import org.usfirst.frc4904.standard.commands.KittenCommand;
+import org.usfirst.frc4904.standard.commands.RunFor;
 import org.usfirst.frc4904.standard.commands.RunIf;
 
 public class RightSideTime extends SideSwitchTime {
 	public RightSideTime() {
 		super();
-		addSequential(new RunIf(new Noop(),
-			RobotMap.gameField.ourSwitch::isRightOurs
-	));
+		addSequential(new KittenCommand(
+			"Is the right switch ours: " + Boolean.toString(RobotMap.gameField.ourSwitch.isRightOurs()), KittenLevel.WTF));
+		addSequential(
+			new RunIf(new RunFor(new IndexerOuttake(AutonConfig.AUTON_OUTTAKE_SPEED), SideSwitchTime.TIME_OUTTAKE), () -> {
+				return RobotMap.gameField.ourSwitch.isRightOurs();
+			}));
 	}
 }
